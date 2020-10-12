1d43b8da933a5b7a79f9bf2e07f0407880710f62
Elizabeth <65304506+EliSauKu@users.noreply.github.com>
2020-10-12 20:02:09 +0200

    Update Capri.py

--- Capri.py
+++ Capri.py
@@ -19,13 +19,13 @@ JAW_COLOR = 'slategray'
 JAW_SHADOW = ''
 BODY_COLOR = 'purple'
 BODY_SHADOW = ''
-#LEG_COLOR = 'purple'
-#LEG_SHADOW = ''
-#TAIL_COLOR = 'purple'
-#TAIL_SHADOW = ''
+LEG_COLOR = 'purple'
+LEG_SHADOW = ''
+TAIL_COLOR = 'purple'
+TAIL_SHADOW = ''
 
 t.pensize(5)
-t.speed(20)
+t.speed(200)
 
 def body():
 
@@ -49,7 +49,6 @@ def body():
     t.forward(54)
     t.left(55)
 
-    
     t.down()
     t.circle(70, 160)
     t.left(175)
@@ -152,14 +151,14 @@ def horns():
     t.forward(50)
     t.right(-10)
     t.circle(60, 80)
-    t.right(90)
+    t.right(94)
     t.circle(-45, 30)
     t.end_fill()
 
     t.begin_fill()
     t.right(155)
     t.up()
-    t.forward(40)
+    t.forward(35)
     t.down()
     t.right(-90)
     t.circle(-55, 50)
@@ -180,19 +179,162 @@ def horns():
     t.forward(15)
     t.end_fill()
 
-#def body2():
+def body2():
 
-    #t.fillcolor(BODY_COLOR)
-    #t.begin_fill()
-    t.left(-258)
+    t.left(-247)
     t.up()
     t.forward(70)
-    #t.end_fill()
+    t.down()
+    t.fillcolor(BODY_COLOR)
+    t.begin_fill()
+    t.circle(-70, 60)
+    t.left(20)
+    t.circle(90, 80)
+    t.right(473)
+    t.end_fill()
+
+    # leg
+    t.fillcolor(LEG_COLOR)
+    t.begin_fill()
+    t.circle(150, 45)
+    t.left(95)
+    t.end_fill()
+    t.circle(200, 30)
+    t.right(15)
+
+    # hoof
+    t.fillcolor("slategray")
+    t.begin_fill()
+    t.forward(35)
+    t.left(115)
+    t.circle(60, 40)
+    t.right(318)
+    t.circle(70, 20)
+    t.right(-125)
+    t.circle(-80, 22)
+    t.end_fill()
+
+    # continuation; legs and rest of the body
+    t.up()
+    t.back(25)
+    t.right(-240)
+    t.down()
+    t.forward(8)
+    t.right(40)
+    t.circle(10, 80)
+    t.right(60)
+    t.forward(40)
+    t.left(-151)
+    t.forward(100)
+    t.right(55)
+    t.circle(120, 30)
+    t.circle(-30, 42)
+
+    t.right(-50)
+    t.up()
+    t.forward(75)
+    t.right(210)
+    t.down()
+    t.begin_fill()
+    t.fillcolor(BODY_COLOR)
+    t.circle(30, 100)
+    t.left(190)
+    t.circle(-38, -150)
+    t.right(180)
+    t.circle(50, 170)   #(radius, length)
+    t.left(70)
+
+    t.up()
+    t.left(-88)
+
+    t.down()
+    t.circle(70, 160)
+    t.left(175)
+    t.circle(-70, -50)
+    t.left(165)
+    t.circle(70, 90)
+    t.right(20)
+    t.circle(70, 30)
+    t.forward(130)
+    t.left(10)
+    t.end_fill()
+    t.circle(-70, 70)
+    t.circle(70, 70)
+    
+    t.end_fill()
+
+def leg():
+
+    t.right(268)
+    t.up()
+    t.forward(198)
+    t.right(100)
+    t.down()
+    t.forward(88)
+    t.right(-80)
+    t.circle(120, 80)
+    t.left(110)
+
+    # hoof
+    t.fillcolor("slategray")
+    t.begin_fill()
+    t.circle(60, 35)
+    t.right(-40)
+    t.forward(15)
+    t.right(-130)
+    t.circle(-60, 25)
+    t.end_fill()
+
+    t.up()
+    t.back(25)
+    t.left(-100)
+    t.down()
+
+    t.forward(10)
+    t.right(100)
+    t.down()
+    t.circle(20, 20)
+    t.right(300)
+    t.forward(15)
+    t.right(50)
+    t.forward(60)
+    t.left(-120)
+    t.fillcolor(LEG_COLOR)
+    t.begin_fill()
+    t.circle(180, 33)
+    t.right(-150)
+    t.circle(-90, 30)
+    t.end_fill()
+
+def tail():
+    t.up()
+    t.right(107)
+    t.forward(280)
+    t.left(60)
+    t.down()
+    t.fillcolor(TAIL_COLOR)
+    t.begin_fill()
+    t.forward(20)
+    t.right(-30)
+    t.circle(50, 120)
+    t.right(130)
+    t.circle(-50, 170)
+    t.left(120)
+    t.circle(-57, 155)
+    t.right(130)
+    t.circle(50, 120)
+    t.right(-10)
+    t.forward(15)
+    t.end_fill()
+
+    
    
 body()
 head()
 eye()
 horns()
-#body2()
+body2()
+leg()
+tail()
 
 t.screen.exitonclick()
